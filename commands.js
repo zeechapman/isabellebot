b55aa5f0b5be43f75dc9905404649a926760f472
zeechapman <zeechapman@gmail.com>
2019-02-23 15:18:46 -0700

    NOW I added outcomes

--- commands.js
+++ commands.js
@@ -217,5 +217,36 @@ module.exports = {
                 msg.channel.send("You rolled a " + ran);
             }
         }
+    },
+    eBall: function(msg) {
+        let outcomes = {
+            "pos": [
+                "It is certain.",
+                "It is decidedly so.",
+                "Without a doubt.",
+                "Yes - definitely.",
+                "You may rely on it.",
+                "As I see it, yes.",
+                "Most likely.",
+                "Outlook good.",
+                "Yes.",
+                "Signs point to yes."
+            ],
+            "neu": [
+                "Reply hazy, try again.",
+                "Ask again later.",
+                "Better not tell you now.",
+                "Cannot predict now.",
+                "Concentrate and ask again."                
+            ],
+            "neg": [
+                "Don't count on it.",
+                "My reply is no.",
+                "My sources say no.",
+                "Outlook not so good.",
+                "Very doubtful."
+            ]
+        };
+        
     }
 }
