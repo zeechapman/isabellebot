e500e8099f28ec8825ca20a349ee21d82ef90bde
zeechapman <zeechapman@gmail.com>
2019-04-03 08:43:33 -0700

    Added error handling. That was easy.

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 /*
     [ ] - Condense code by adding functions for some actions
     [ ] - Condense code by adding cooldown function control
-    [ ] - Remove references when platformed on Heroku (express, Procfile, etc)
+    [ ] - Remove references when platformed on Heroku (Procfile, etc)
+        - Once local server is set up, remove it, anyways.
     [X] - Try to make being able to tell from ! and !sa in one function instead of seperating it (not possible at the moment)
-    [ ] - Try to automate the process of adding commands.
+    [X] - Try to automate the process of adding commands.
     [ ] - Incorporate MongoDB
+    [ ] - Error handle if command does not exist
 */
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -14,7 +16,7 @@ const token = require('./data');
 
 // When the bot is on, prepare
 client.on('ready', () => {
-    console.log("Isabelle Bot is on, and ready to go!");
+    console.log("\nIsabelle Bot is on, and ready to go!\n");
     client.user.setActivity("the mayor", { type: "LISTENING" });
 });
 
@@ -26,10 +28,14 @@ client.on('message', (msg) => {
         return;
     }
 
-    if (msg.content.startsWith("!sa ")) {
-        processCommand(msg, 4, isaCommands);
-    } else if (msg.content.startsWith("!")) {
-        processCommand(msg, 1, regCommands);
+    try {
+        if (msg.content.startsWith("!sa ")) {
+            processCommand(msg, 4, isaCommands);
+        } else if (msg.content.startsWith("!")) {
+            processCommand(msg, 1, regCommands);
+        }
+    } catch (err) {
+        console.log("Bad command: " + msg.content);
     }
 
 });
