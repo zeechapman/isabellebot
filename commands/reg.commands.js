a655ba9dc7d38c168ee1274b0246460240e35f28
zeechapman <zeechapman@gmail.com>
2019-04-02 23:51:11 -0700

    Got coolDownControl working. 8ball working. On to stab

--- commands/reg.commands.js
+++ commands/reg.commands.js
@@ -4,9 +4,52 @@ let emotes = {
     "thisisfine": "<:thisisfine:467198644823654402>"
 }
 
-// function coolDownControl(cdBool, cdLast)
+// Cooldowns
+let ballCD = {
+    onCD: false,
+    cdLast: new Date().getTime()
+}
+
+let stabCD = {
+    onCD: false,
+    cdLast: new Date().getTime()
+}
 
 let critRoll = false;
+
+
+
+/**
+ * Cooldown control
+ * @param {Discord.Client()} msg Discord
+ * @param {Object} cdObj The object that contains the variables
+ * @param {String} cdMsg The message to display if the cooldown is still in effect
+ * @param {Number} cdTime (seconds) How long the cooldown lasts
+ * @param {function} fn The function to call when everything is okay
+ */
+function coolDownControl(msg, cdObj, cdMsg, cdTime, fn) {
+    let cdNow = new Date().getTime();
+    let cdBetween = cdNow - cdObj.cdLast;
+    if (cdObj.onCD === false) {
+        cdObj.onCD = true;
+        fn();
+    } else if (cdObj.onCD === true) {
+        if (cdBetween <= (cdTime * 1000)) {
+            let timeRemaining = (cdTime * 1000) - cdBetween;
+            let timeStr = timeRemaining.toString();
+            if (timeRemaining < 10000) {
+                msg.channel.send(cdMsg + "\nTime remaining: " + timeStr.substr(0, 1));
+            } else {
+                msg.channel.send(cdMsg + "\nTime remaining: " + timeStr.substr(0, 2));
+            }
+        }
+        else {
+            cdObj.cdLast = new Date().getTime();
+            fn();
+        }
+    }
+}
+
 
 exports.module = {
     "caw": {
@@ -88,4 +131,52 @@ exports.module = {
             msg.channel.send("Ba dum tss. What a knee slapper! (☞ﾟヮﾟ)☞");
         }
     },
+    "8ball": {
+        fn: (msg, args) => {
+            let outcomes = {
+                "pos": [
+                    "It is certain.",
+                    "It is decidedly so.",
+                    "Without a doubt.",
+                    "Yes - definitely.",
+                    "You may rely on it.",
+                    "As I see it, yes.",
+                    "Most likely.",
+                    "Outlook good.",
+                    "Yes.",
+                    "Signs point to yes."],
+                "neu": [
+                    "Reply hazy, try again.",
+                    "Ask again later.",
+                    "Better not tell you now.",
+                    "Cannot predict now.",
+                    "Concentrate and ask again."],
+                "neg": [
+                    "Don't count on it.",
+                    "My reply is no.",
+                    "My sources say no.",
+                    "Outlook not so good.",
+                    "Very doubtful."]
+            };
+
+            let command = () => {
+                let ranIn = Math.floor(Math.random() * 12);
+                let desc = "";
+
+                if (ranIn <= 4) { // If number is between 0 and 1
+                    let i = Math.floor(Math.random() * outcomes.pos.length);
+                    desc = outcomes.pos[i];
+                } else if (ranIn >= 5 && ranIn <= 9) { // If number is between 2 and 3
+                    let i = Math.floor(Math.random() * outcomes.neu.length);
+                    desc = outcomes.neu[i];
+                } else if (ranIn >= 10 && ranIn <= 11) { // If number is between 4 and 5
+                    let i = Math.floor(Math.random() * outcomes.neg.length);
+                    desc = outcomes.neg[i];
+                }
+                msg.channel.send('"' + args + '"\n8-Ball says: *' + desc + '*');
+            }
+
+            coolDownControl(msg, ballCD, "Sorry, the Magic 8Ball needs time to cool down!", 30, command);
+        }
+    }
 }
