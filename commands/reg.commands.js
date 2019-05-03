f263481b2af0b262f9cd9c17842e8256ff5004c6
zeechapman <zeechapman@gmail.com>
2019-05-02 21:32:31 -0700

    Added happy command. Need to add more songs

--- commands/reg.commands.js
+++ commands/reg.commands.js
@@ -4,8 +4,8 @@ const database = require('./firebase/firebase');
 
 // List of useable emotes
 let emotes = {
-    "caw": '<:caw:477160191029280769>',
-    "thisisfine": "<:thisisfine:467198644823654402>"
+    'caw': '<:caw:477160191029280769>',
+    'thisisfine': '<:thisisfine:467198644823654402>'
 }
 
 // Cooldowns
@@ -19,7 +19,7 @@ let stabCD = {
     cdLast: new Date().getTime()
 }
 
-let stabIndex = Math.floor(Math.random() * 3); // The index of the current stab image
+let stabIndex = Math.floor(Math.random() * 5); // The index of the current stab image
 let critRoll = false;
 let imgPath = 'https://raw.githubusercontent.com/zeechapman/isabellebot/dev/img/';
 
@@ -44,9 +44,9 @@ function coolDownControl(msg, cdObj, cdMsg, cdTime, fn) {
             let timeRemaining = (cdTime * 1000) - cdBetween;
             let timeStr = timeRemaining.toString();
             if (timeRemaining < 10000) {
-                msg.channel.send(cdMsg + "\nTime remaining: " + timeStr.substr(0, 1));
+                msg.channel.send(cdMsg + '\nTime remaining: ' + timeStr.substr(0, 1));
             } else {
-                msg.channel.send(cdMsg + "\nTime remaining: " + timeStr.substr(0, 2));
+                msg.channel.send(cdMsg + '\nTime remaining: ' + timeStr.substr(0, 2));
             }
         }
         else {
@@ -58,53 +58,53 @@ function coolDownControl(msg, cdObj, cdMsg, cdTime, fn) {
 
 
 exports.module = {
-    "caw": {
+    'caw': {
         fn: msg => {
-            msg.channel.send("Caw caw, baby! " + emotes.caw);
+            msg.channel.send('Caw caw, baby! ' + emotes.caw);
         }
     },
-    "fliptable": {
+    'fliptable': {
         fn: msg => {
             let faces = ['(╯°□°）╯︵ ┻━┻', '(╯˘ ᵕ˘）╯︵ ┻━┻', '(╯˘꒳˘）╯︵ ┻━┻', '(/¯◡ ‿ ◡)/¯ ~ ┻━┻', '┬─┬ ︵ /(.□. ﾉ）(wait wut)'];
             let faceChoice = faces[Math.floor(Math.random() * faces.length)]
             msg.channel.send(faceChoice);
         }
     },
-    "fixtable": {
+    'fixtable': {
         fn: msg => {
-            msg.channel.send("┬──┬ ノ( ゜-゜ノ)");
+            msg.channel.send('┬──┬ ノ( ゜-゜ノ)');
         }
     },
-    "phil": {
+    'phil': {
         fn: msg => {
-            msg.channel.send("Needs more Phil");
+            msg.channel.send('Needs more Phil');
         }
     },
-    "rip": {
+    'rip': {
         fn: msg => {
-            msg.channel.send("Press F to pay respects");
+            msg.channel.send('Press F to pay respects');
         }
     },
-    "trip": {
+    'trip': {
         fn: msg => {
-            msg.channel.send("Press T to pay respects to Josh Jr.");
+            msg.channel.send('Press T to pay respects to Josh Jr.');
         }
     },
-    "nani": {
+    'nani': {
         fn: msg => {
-            msg.channel.send("*" + msg.member + " steps back in shock.*\nNANI??");
+            msg.channel.send('*' + msg.member + ' steps back in shock.*\nNANI??');
         }
     },
-    "hug": {
+    'hug': {
         fn: (msg, args) => {
             if (args.length > 0) {
-                msg.channel.send(msg.member + " gives a big hug to " + args + "\n(.づ◡﹏◡)づ.");
+                msg.channel.send(msg.member + ' gives a big hug to ' + args + '\n(.づ◡﹏◡)づ.');
             } else {
-                msg.channel.send(msg.member + " gives a big hug to everyone!\n(.づ◡﹏◡)づ.");
+                msg.channel.send(msg.member + ' gives a big hug to everyone!\n(.づ◡﹏◡)づ.');
             }
         }
     },
-    "sawthat": {
+    'sawthat': {
         fn: msg => {
             let preMsg = ['I saw that edit! ', 'Is that a...NINJA EDIT? ', 'Nice edit you did there...'];
             let lennys = ['(͠≖ ͜ʖ͠≖)', '( ͡~ ͜ʖ ͡°)', '( ͡◉ ͜ʖ ͡◉)', '( ͡° ͜ʖ ͡°)']
@@ -113,48 +113,48 @@ exports.module = {
             msg.channel.send(preMsg[pre] + lennys[len]);
         }
     },
-    "thisisfine": {
+    'thisisfine': {
         fn: msg => {
-            let fire = ":fire:";
+            let fire = ':fire:';
             msg.channel.send(fire + emotes.thisisfine + fire);
         }
     },
-    "badum": {
+    'badum': {
         fn: msg => {
-            msg.channel.send("Ba dum tss. What a knee slapper! (☞ﾟヮﾟ)☞");
+            msg.channel.send('Ba dum tss. What a knee slapper! (☞ﾟヮﾟ)☞');
         }
     },
-    "8ball": {
+    '8ball': {
         fn: (msg, args) => {
             let outcomes = {
-                "pos": [
-                    "It is certain.",
-                    "It is decidedly so.",
-                    "Without a doubt.",
-                    "Yes - definitely.",
-                    "You may rely on it.",
-                    "As I see it, yes.",
-                    "Most likely.",
-                    "Outlook good.",
-                    "Yes.",
-                    "Signs point to yes."],
-                "neu": [
-                    "Reply hazy, try again.",
-                    "Ask again later.",
-                    "Better not tell you now.",
-                    "Cannot predict now.",
-                    "Concentrate and ask again."],
-                "neg": [
-                    "Don't count on it.",
-                    "My reply is no.",
-                    "My sources say no.",
-                    "Outlook not so good.",
-                    "Very doubtful."]
+                'pos': [
+                    'It is certain.',
+                    'It is decidedly so.',
+                    'Without a doubt.',
+                    'Yes - definitely.',
+                    'You may rely on it.',
+                    'As I see it, yes.',
+                    'Most likely.',
+                    'Outlook good.',
+                    'Yes.',
+                    'Signs point to yes.'],
+                'neu': [
+                    'Reply hazy, try again.',
+                    'Ask again later.',
+                    'Better not tell you now.',
+                    'Cannot predict now.',
+                    'Concentrate and ask again.'],
+                'neg': [
+                    'Don\'t count on it.',
+                    'My reply is no.',
+                    'My sources say no.',
+                    'Outlook not so good.',
+                    'Very doubtful.']
             };
 
             let command = () => {
                 let ranIn = Math.floor(Math.random() * 12);
-                let desc = "";
+                let desc = '';
 
                 if (ranIn <= 4) { // If number is between 0 and 1
                     let i = Math.floor(Math.random() * outcomes.pos.length);
@@ -169,10 +169,10 @@ exports.module = {
                 msg.channel.send('"' + args + '"\n8-Ball says: *' + desc + '*');
             }
 
-            coolDownControl(msg, ballCD, "Sorry, the Magic 8Ball needs time to cool down!", 30, command);
+            coolDownControl(msg, ballCD, 'Sorry, the Magic 8Ball needs time to cool down!', 30, command);
         }
     },
-    "stab": {
+    'stab': {
         fn: msg => {
             let imgs = ['pic0.png', 'pic1.gif', 'pic2.png', 'pic3.png', 'pic4.png', 'pic5.gif', 'pic6.png', 'pic7.png'];
             let command = () => {
@@ -182,22 +182,22 @@ exports.module = {
                 } else stab = 0;
                 msg.channel.send(embed);
             }
-            coolDownControl(msg, stabCD, "I can't PRESSURE too quickly!", 60, command);
+            coolDownControl(msg, stabCD, 'I can\'t PRESSURE too quickly!', 60, command);
         }
     },
-    "stop": {
+    'stop': {
         fn: msg => {
             let embed = new Discord.RichEmbed().setImage(imgPath + 'stop.gif');
             msg.channel.send(embed);
         }
     },
-    "cati": {
+    'cati': {
         fn: msg => {
             let embed = new Discord.RichEmbed().setImage(imgPath + 'illumicati.gif');
             msg.channel.send(embed);
         }
     },
-    "strike": {
+    'strike': {
         /**
          * Give a mentioned user a strike
          */
@@ -211,7 +211,7 @@ exports.module = {
             for (let i = 0; i < ids.length; i++) {
                 if (ids[i] === msg.author.id) {
                     match = true;
-                    console.log("Matched");
+                    console.log('Matched');
                 }
             }
             // If the user who did the strike matched, allow them to perform the strike
@@ -221,7 +221,7 @@ exports.module = {
                         return val.name === 'staff_room';
                     });
                     msg.delete();
-                    staffChannel.send("Oops, looks like a little accident happened! You got to make sure to include a @user when issuing a strike.\nAs a reminder, the command is:\n\`\`\`!strike <@username#0000> Reason (optional).\`\`\`\nAlso, post it in #staff_room so no one can be yelled at specifically for issuing a strike. Thank you :)");
+                    staffChannel.send('Oops, looks like a little accident happened! You got to make sure to include a @user when issuing a strike.\nAs a reminder, the command is:\n\`\`\`!strike <@username#0000> Reason (optional).\`\`\`\nAlso, post it in #staff_room so no one can be yelled at specifically for issuing a strike. Thank you :)');
 
                 } else {
                     tag = msg.mentions.users.first().tag;
@@ -234,19 +234,32 @@ exports.module = {
                 setTimeout(() => {
                     // If not, delete the message. Pretend it never happened.
                     msg.delete();
-                    console.log("Unauthorized used of !strike. Removing.");
+                    console.log('Unauthorized used of !strike. Removing.');
                 }, 100);
             }
         }
     },
-    "snap": {
+    'snap': {
         fn: msg => {
             let phrase = 'feel so good';
             let spaced = '';
             for (let i = 0; i < phrase.length; i++) {
-                spaced += phrase[i] + " ".repeat(i);
+                spaced += phrase[i] + ' '.repeat(i);
             }
-            msg.channel.send("Ummm, " + msg.member + ", I don't " + spaced);
+            msg.channel.send('Ummm, ' + msg.member + ', I don\'t ' + spaced);
+        }
+    },
+    'happy': {
+        fn: msg => {
+            // List of random songs to select from
+            let songs = [
+                'https://www.youtube.com/watch?v=ZbZSe6N_BXs',
+                'https://www.youtube.com/watch?v=s7dTBoW5H9k',
+                'https://www.youtube.com/watch?v=L3HQMbQAWRc'
+            ]
+            let len = songs.length;
+            let ran = Math.floor(Math.random() * len);
+            msg.channel.send('Oh! Someone need a happy song? I got one!\n' + songs[ran]);
         }
     }
 }
