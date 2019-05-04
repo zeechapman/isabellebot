d3add10bc376cf964172e0adcd606562f54ff81f
zeechapman <zeechapman@gmail.com>
2019-05-04 03:10:53 -0700

    Added seperate file for song list

---
+++ commands/songs.js
@@ -0,0 +1,60 @@
+// To make things easier, and less cluttered,
+// here's a list of songs for the !happy command
+module.exports = [
+    {
+        artist: "Pharrell Williams",
+        song: "Happy",
+        link: "https://www.youtube.com/watch?v=ZbZSe6N_BXs"
+    },
+    {
+        artist: "Bob Marley",
+        song: "Don't Worry",
+        link: "https://www.youtube.com/watch?v=L3HQMbQAWRc"
+    },
+    {
+        artist: "Wind Waker Soundtrack",
+        song: "Dragon Roost Island",
+        link: 'https://www.youtube.com/watch?v=wRWq53IFXVQ'
+    },
+    {
+        artist: "A Hat in Time Soundtrack",
+        song: "Peace and Tranquility",
+        link: "https://www.youtube.com/watch?v=SHvhps47Lmc"
+    },
+    {
+        artist: "Initial D.",
+        song: "Running in the 90's",
+        link: "https://youtu.be/BJ0xBCwkg3E"
+    },
+    {
+        artist: "Red Hot Chili Peppers",
+        song: "Roller Coaster of Love",
+        link: "https://youtu.be/uUNN0B7zPi0"
+    },
+    {
+        artist: "Paul Simon",
+        song: "Kodachrome",
+        link: "https://youtu.be/8rlDTK6QI-w"
+    },
+    {
+        artist: "Video Game Remixes",
+        song: "Stronger Than You (Remix)",
+        link: "https://www.youtube.com/watch?v=QRmkyWVrpKQ"
+    },
+    {
+        artist: "M. Bulteau",
+        song: "Boneklezmer",
+        link: "https://www.youtube.com/watch?v=GIuBpl7k_eU"
+    },
+    {
+        artist: "Smooth McGroove",
+        song: "Spider Dance Acapella",
+        link: "https://www.youtube.com/watch?v=OUBq3PIOJ8k"
+    },
+    {
+        artist: "Red Hot Chili Peppers",
+        song: "Tell Me Baby",
+        link: "https://www.youtube.com/watch?v=BWyblTqAwp0"
+
+    }
+]
