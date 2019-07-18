1f0cece8e4591659566df4d4fe00296b0f42dd6c
zeechapman <zeechapman@gmail.com>
2019-07-18 09:37:55 -0700

    Slowly adding profile picture

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ client.on('message', msg => {
         } else if (msg.content.startsWith("im ") || msg.content.startsWith("I'm ") || msg.content.startsWith("i'm ")) {
             let str = msg.content.split(' '); // Split the string
             let conStr = ''; // Blank string to 'string' together (ha, get it? Dad joke)
+            let embed = new Discord.RichEmbed().setThumbnail('');
             for (let i = 1; i < str.length; i++) {
                 // If the end is reached, don't add a space
                 if (i === str.length - 1) {
