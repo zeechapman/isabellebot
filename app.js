049ba9252068add16513e8fc0e61566d1c9b1263
zeechapman <zeechapman@gmail.com>
2019-07-18 10:10:06 -0700

    Prevented someone else from calling themselves Dad

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ client.on('message', msg => {
         } else if (msg.content.startsWith("im ") || msg.content.startsWith("I'm ") || msg.content.startsWith("i'm ")) {
             let str = msg.content.split(' '); // Split the string
             let conStr = ''; // Blank string to 'string' together (ha, get it? Dad joke)
+            let result = ''; // Another blank
             for (let i = 1; i < str.length; i++) {
                 // If the end is reached, don't add a space
                 if (i === str.length - 1) {
@@ -59,9 +60,15 @@ client.on('message', msg => {
                     conStr += str[i] + ' ';
                 }
             }
+            let embed = new Discord.RichEmbed();
             // Send it
-            let embed = new Discord.RichEmbed().setThumbnail('https://raw.githubusercontent.com/zeechapman/isabellebot/dev/img/dadbot-thumbnail.png').setDescription('Hi, ' + conStr + ". I'm Dad!").setFooter("~Dad Bot");
-            msg.channel.send(embed);
+            if (conStr.toUpperCase() === 'DAD') {
+                console.log("Dad detected!");
+                result = "No you're not. I'm Dad!";
+            } else {
+                result = 'Hello, ' + conStr + ". I'm Dad!";
+            }
+            msg.channel.send(embed.setThumbnail('https://raw.githubusercontent.com/zeechapman/isabellebot/dev/img/dadbot-thumbnail.png').setDescription(result).setFooter("~Dad Bot"));
         }
     } catch (err) {
         console.log("Bad command, or an error has happened.\nError: " + err + "\n");
