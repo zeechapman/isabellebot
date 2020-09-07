f939a50b02c7c45122d6a93d96732bc426e8bcff
zeechapman <zeechapman@gmail.com>
2020-09-07 00:21:31 -0700

    Altered getTime function, added processCmd to misc

--- misc.js
+++ misc.js
@@ -5,15 +5,33 @@ function getTime() {
     let date = new Date();
     let hrs = date.getHours();
     let min = date.getMinutes();
-    let time;
-
-    if (hrs > 12) {
-        time = (hrs - 12) + ':' + min + ' PM';
-    } else {
-        time = hrs + ':' + min + ' AM';
-    }
+    let ampm = hrs >= 12 ? 'PM' : 'AM';
+    hrs = hrs % 12;
+    hrs = hrs ? hours : 12; // If 0, then set it to 12
+    let time = hrs + ':' + min + ' ' + ampm;
 
     return time;
 }
 
 exports.getTime = getTime;
+
+/**
+ * Process message as a command
+ * @param {string} msg 
+ */
+function processCmd(msg) {
+    let full = msg.content.substr(1); // Remove the '!'
+    let split = full.split(" "); // Split each part of a string by the space and store into array
+    let cmd = split[0]; // Ex: '!caw' will be 'caw'
+
+    /*  Get the argument by splitting into an array after the command.
+        Ex: !hug John, the Destroyer -> John,,the,Destroyer
+        Afterwards, using ReGex, replace twice to make it: John, the Destroyer */
+    let args = split.slice(1);
+    let argsJoin = args.join().replace(/,{2}/g, ', ').replace(/,\b/g, ' ');
+
+    console.log(`Command read: \x1b[32m${msg.content}\x1b[0m at \x1b[36m${getTime()}`);
+    msg.channel.send(commands[cmd]());
+}
+
+exports.processCmd = processCmd;
